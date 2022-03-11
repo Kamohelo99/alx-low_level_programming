29ab9d40cdfcf493945f291901af850a0a639bf3
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-11 11:36:02 -0800

    Generate random number and print if it's +,-,0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%dis positive\n", n);
+	else if (n == 0)
+		printf("%dis zero\n", n);
+	else
+		printf("%dis negative\n", n);
+	return (0);
+}
