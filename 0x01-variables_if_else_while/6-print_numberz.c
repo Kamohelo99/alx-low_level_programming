88709c3d5b9fcc357baee8a7edc6640321867e59
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-13 05:21:16 -0700

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+/**
+ * main - starting point for program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+
+	putchar('\n');
+	return (0)
+}
