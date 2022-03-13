f156a8839043ef5a274e843413bd7c986bfc7295
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-13 04:13:12 -0700

    Print lower and uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        char character;
+
+        for (character = 'a'; character <= 'z'; character++)
+        {
+                putchar(character);
+        }
+	for (character = 'A'; character <= 'Z'; character++)
+        {
+                putchar(character);
+        }
+        putchar('\n');
+        return (0);
+}
+
