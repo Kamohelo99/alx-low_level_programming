6ad7bbc18e6b7c168fc8a25d33987eb093fd27a4
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-15 12:51:06 -0700

    This program prints letters of the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabets a to z 10 times
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+
+	int count = 0;
+
+	while (count < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		ch = 'a';
+
+		count++;
+	}
+}
