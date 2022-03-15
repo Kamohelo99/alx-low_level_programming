f1ea9fab8476446ec8fa6bdfe133214a36a87775
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-15 12:10:18 -0700

    Program that prints a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * Main - Program entry point
+ *
+ * Return : 0
+ */
+
+int main (void)
+{
+	char str = "_putchar";
+
+	while (str[i] != '\0')
+	{
+		printf(str[i]);
+		i++;
+	}
+	printf("\n");
+
+}
