951a00c36282e4a5ea3549d87dade2d19653d827
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-15 12:59:54 -0700

    Program detect if a character is a low or uppercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/**
+* _islower - checks if a character is low case
+*
+* Return: 1 if the letter is lowercase, otherwise 0
+*/
+
+int _islower(int c)
+{
+	/*ascii code of lowcase letters ranges from 97-122)*/
+	if (c >= 97 && c <= 122)
+		return (1);
+
+	return (0);
+}
