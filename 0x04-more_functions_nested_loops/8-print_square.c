741a9353bd2b57d93f34fb1d83b8a9465d831921
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-17 12:37:05 -0700

    modified print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,24 @@
 #include "main"
 
-/**
- * print_square - Print the last digit
- * @size: Number that is going to be splited
- * Return: Return the last digit
+/** print_square - Print square.
+ * @size: Size of square.
+ * Return: Square.
  */
+
 void print_square(int size)
 {
-	int a, b;
+	int y, x;
 
 	if (size <= 0)
-		_putchar ('\n');
-	else
 	{
-		for (a = 0; a < size; a++)
+		_putchar('\n');
+	}
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
 		{
-			for (b = 0; b < size; b++)
-			{
-				_putchar ('#');
-			}
-			_putchar ('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 }
