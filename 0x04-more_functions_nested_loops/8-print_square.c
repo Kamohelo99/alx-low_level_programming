e0f26852698dd49a3b2e0b07b1f40edd62369442
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-17 12:33:37 -0700

    a function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * print_square - Print the last digit
+ * @size: Number that is going to be splited
+ * Return: Return the last digit
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	}
+}
