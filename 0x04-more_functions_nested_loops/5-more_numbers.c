51715d8e17ffc1e0a0aabe9babb72a524ff42d63
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-17 11:50:13 -0700

    modified main.h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
-/**
- * more_numbers - Print 0 to 14 , 10 times
- * Return: Return the last digit
- */
+#include <stdio.h>
 
+/**
+ * more_numbers - Print numbers from 0 to 14, 10 times.
+ * Return: nothing.
+ */
 void more_numbers(void)
 {
-	int x, int y;
+	int times, numbers;
 
-	for (y = 0; y < 10; y++)
+	for (times = 0; times < 10; times++)
 	{
-		for (x = 0; x <= 14; x++)
+		for (numbers = 0; numbers <= 14; numbers++)
 		{
-			if (x > 9)
-			{
-				_putchar (x / 10 + '0');
-			}
-			_putchar (x % 10 + '0');
+			if (numbers >= 10)
+				putchar((numbers / 10) + '0');
+
+			putchar((numbers % 10) + '0');
 		}
-		_putchar ('\n');
+		putchar('\n');
 	}
 }
