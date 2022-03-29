c450f5a457dca05fcbaf6b6b4f6aa185f2ea786e
Kamohelo99 <kamohelo0329@gmail.com>
2022-03-29 14:44:41 -0700

    Corrected strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * strchr - Locates a character in a string
+ *_strchr - Locates a character in a string
  *
  * @s: The string we will be looking for the character from
  * @c: The character we are looking for
  *
- * Return : A pointer to the first occurrence of @c
- * in the string s, or null if not found
+ * Return: A pointer to the first occurrence of @c
+ *          in the string s, or null if not found
  */
 
 char *_strchr(char *s, char c)
